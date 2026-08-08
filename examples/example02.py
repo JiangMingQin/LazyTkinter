01e72464bd1d753f4c42ac6ac368b21c446b1b6a
MingQin J <3315536229@qq.com>
2026-08-09 02:23:26 +0800

    refactor: finish example02 adaptation to latest API with chain fills and font kwargs

--- examples/example02.py
+++ examples/example02.py
@@ -39,78 +39,76 @@ class App:
         app = ltk.Application()
 
         # build page
-        app.size("large").window_title("My first app").column(
-            ltk.Column().gap(5).padding(5).height().fill().add(
-                # Top Bar
-                ltk.Row().gap(10).padding(10).align("center").height(60).add(
-                    ltk.Label().text("LazyTkinter"),
-                    ltk.Empty().width(10),  # fixed placeholder
-                    ltk.Entry().width().fill().height(35).radius(100).placeholder_text("entry..."),
-                    ltk.Button().width(80).text("search").event(self.search_something),
+        app.size("large").window_title("My first app").gap(5).padding(10).column(
+            # Top Bar
+            ltk.Row().gap(10).padding(10).align("center").height(60).width().fill().add(
+                ltk.Label().text("LazyTkinter").font(family="Arial", size=16, weight="bold"),
+                ltk.Empty().width(10),  # fixed placeholder
+                ltk.Entry().width().fill().height(35).radius(100).placeholder_text("entry..."),
+                ltk.Button().width(80).text("search").event(self.search_something),
+            ),
+            # Main Area
+            ltk.Row().gap(10).padding(5).height().fill().add(
+                # Left Sidebar
+                ltk.Column().width(150).gap(10).padding(10).height().fill().add(
+                    ltk.Button().height(30).text("Page 1").event(lambda: self.turn_to_page(1)),
+                    ltk.Button().height(30).text("Page 2").event(lambda: self.turn_to_page(2)),
+                    ltk.Button().height(30).text("Disabled").state("disabled"),
+                    ltk.Spacer(),
+                    ltk.Switch().height(30).radius(10).text("Dark/Light").event(self.switch_mode),
                 ),
-                # Main Area
-                ltk.Row().gap(10).padding(5).height().fill().add(
-                    # Left Sidebar
-                    ltk.Column().width(150).gap(10).padding(10).height().fill().add(
-                        ltk.Button().height(30).text("Page 1").event(lambda: self.turn_to_page(1)),
-                        ltk.Button().height(30).text("Page 2").event(lambda: self.turn_to_page(2)),
-                        ltk.Button().height(30).text("Disabled").state("disabled"),
-                        ltk.Spacer(),
-                        ltk.Switch().height(30).radius(10).text("Dark/Light").event(self.switch_mode),
+                # Right Main Content
+                ltk.Column().gap(10).padding(10).height().fill().width().fill().add(
+                    ltk.Row().height(130).width().fill().gap(10).add(
+                        # Checkbox
+                        ltk.Column().width(60).gap(5).add(
+                            ltk.CheckBox().height(20).radius(8),
+                            ltk.CheckBox().height(20).radius(8),
+                            ltk.CheckBox().height(20).radius(8),
+                            ltk.CheckBox().height(20).radius(8),
+                        ),
+                        # RadioButton
+                        ltk.Column().width(60).gap(5).add(
+                            ltk.RadioButton().height(20),
+                            ltk.RadioButton().height(20),
+                            ltk.RadioButton().height(20),
+                        ),
+                        # Textbox
+                        ltk.Textbox().width().fill().height().fill().radius(8),
                     ),
-                    # Right Main Content
-                    ltk.Column().gap(10).padding(10).height().fill().add(
-                        ltk.Row().height(130).gap(10).add(
-                            # Checkbox
-                            ltk.Column().width(60).gap(5).add(
-                                ltk.CheckBox().height(20).radius(8),
-                                ltk.CheckBox().height(20).radius(8),
-                                ltk.CheckBox().height(20).radius(8),
-                                ltk.CheckBox().height(20).radius(8),
-                            ),
-                            # RadioButton
-                            ltk.Column().width(60).gap(5).add(
-                                ltk.RadioButton().height(20),
-                                ltk.RadioButton().height(20),
-                                ltk.RadioButton().height(20),
-                            ),
-                            # Textbox
-                            ltk.Textbox().width().fill().height().fill().radius(8),
-                        ),
-                        # Slider
-                        ltk.Slider().height(20),
-                        # ProgressBar
-                        ltk.ProgressBar().value(0.7).height(10),
-                        # SegmentedButton
-                        ltk.Row().height(30).add(
-                            ltk.SegmentedButton().radius(100).values([
-                                "Option A",
-                                "Option B",
-                                "Option C",
-                            ]).event(self.on_segment_click).set_value("Option A"),
-                        ),
-                        # ComboBox & OptionMenu
-                        ltk.Row().height(30).gap(10).add(
-                            ltk.ComboBox().width().fill().values([
-                                "Combo 1",
-                                "Combo 2",
-                            ]).event(self.on_combo_change),
-                            ltk.OptionMenu().width().fill().values([
-                                "Menu 1",
-                                "Menu 2",
-                            ]),
-                        ),
-                        # Scrollable list
-                        ltk.Scroll(
-                            ltk.Column().gap(5).add(
-                                ltk.Button().radius(10).text("Item 1"),
-                                ltk.Button().radius(10).text("Item 2"),
-                                ltk.Button().radius(10).text("Item 3"),
-                                ltk.Button().radius(10).text("Item 4"),
-                                ltk.Button().radius(10).text("Item 5"),
-                                ltk.Button().radius(10).text("Item 6"),
-                                ltk.Button().radius(10).text("Item 7"),
-                            ),
+                    # Slider
+                    ltk.Slider().height(20).width().fill(),
+                    # ProgressBar
+                    ltk.ProgressBar().value(0.7).height(10).width().fill(),
+                    # SegmentedButton
+                    ltk.Row().height(30).add(
+                        ltk.SegmentedButton().radius(100).values([
+                            "Option A",
+                            "Option B",
+                            "Option C",
+                        ]).event(self.on_segment_click).set_value("Option A"),
+                    ),
+                    # ComboBox & OptionMenu
+                    ltk.Row().height(30).gap(10).add(
+                        ltk.ComboBox().width().fill().values([
+                            "Combo 1",
+                            "Combo 2",
+                        ]).event(self.on_combo_change),
+                        ltk.OptionMenu().width().fill().values([
+                            "Menu 1",
+                            "Menu 2",
+                        ]),
+                    ),
+                    # Scrollable list
+                    ltk.Scroll(
+                        ltk.Column().gap(5).add(
+                            ltk.Button().radius(10).text("Item 1"),
+                            ltk.Button().radius(10).text("Item 2"),
+                            ltk.Button().radius(10).text("Item 3"),
+                            ltk.Button().radius(10).text("Item 4"),
+                            ltk.Button().radius(10).text("Item 5"),
+                            ltk.Button().radius(10).text("Item 6"),
+                            ltk.Button().radius(10).text("Item 7"),
                         ),
                     ),
                 ),
